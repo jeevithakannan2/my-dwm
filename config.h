84b5ccaa2e45bac788fa0829be87fcad027d5720
Jeevitha Kanann K S <ksjeevithakannan123@gmail.com>
2024-06-14 23:45:10 +0530

    Added patches for hide unactive tags, winicon

--- config.h
+++ config.h
@@ -10,6 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+#define ICONSIZE 16   /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
 static const char *fonts[]          = { "Noto Sans Nerd:size=14" };
 static const char dmenufont[]       = "Noto Sans Nerd:size=14";
 
@@ -63,7 +65,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 static const char *const autostart[] = {
-	"/home/jeeva/dwmstatus/dwmstatus", NULL,
+	"dwmstatus", NULL,
 	NULL
 };
 
